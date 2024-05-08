9e3b0138e757f25e0ceafda5033be1be8b7bfb9d
SilviaEzra <159856417+SilviaEzra@users.noreply.github.com>
2024-05-08 18:38:49 +0200

    ok4

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { PresupuestosComponent } from '../presupuestos/presupuestos.component';
 
@@ -9,10 +10,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent, PresupuestosComponent]
-    })
-    .compileComponents();
-    
+      declarations: [HomeComponent, PresupuestosComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [ChangeDetectorRef]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,4 +23,24 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit precioFinalChange event', () => {
+    spyOn(component.precioFinalChange, 'emit');
+    component.emitirPrecioFinal();
+    expect(component.precioFinalChange.emit).toHaveBeenCalled();
+  });
+
+  it('should calculate precio final', () => {
+    component.formulario = new FormGroup({
+      "Seo": new FormControl(true),
+      "Ads": new FormControl(false),
+      "Web": new FormControl(true)
+    });
+    component.numeroDePaginas = 2;
+    component.numeroDeIdiomas = 3;
+    const precioFinal = component.calcularPrecioFinal();
+    expect(precioFinal).toEqual(2 * 3 * 30 + component.precios['Seo'] + component.precios['Web']);
+  });
+
+  // Agrega más pruebas según sea necesario
 });
